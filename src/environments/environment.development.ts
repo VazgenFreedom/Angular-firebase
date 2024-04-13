99a5649f0a01df111cd44192d3f4bff55c3ba5e1
VazgenYeghiazaryan <115686860+VazgenYeghiazaryan@users.noreply.github.com>
2024-04-13 17:20:25 +0400

    Added firebase

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAZc6c9MKbCID8foNkc9DI_dE2v--MEevU',
+    authDomain: 'angular-firebase-freedom.firebaseapp.com',
+    databaseURL: 'https://angular-firebase-freedom-default-rtdb.firebaseio.com',
+    projectId: 'angular-firebase-freedom',
+    storageBucket: 'angular-firebase-freedom.appspot.com',
+    messagingSenderId: '246125201963',
+    appId: '1:246125201963:web:944dcd1ff80afd06c3f86e',
+    measurementId: 'G-WLQC6KF8RN',
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,32 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { initializeApp } from 'firebase/app';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from './environments/environment.development';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  provideRouter,
+  withPreloading,
+  PreloadAllModules,
+} from '@angular/router';
+//import { APP_ROUTES } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      AngularFireModule.initializeApp(environment.firebaseConfig),
+      AngularFireDatabaseModule,
+      AngularFireAuthModule,
+      AngularFirestore,
+      BrowserAnimationsModule
+    ),
+    // provideRouter(APP_ROUTES, withPreloading(PreloadAllModules)),
+  ],
+});
